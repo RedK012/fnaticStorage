38a00a3b97bb04f329d391838b4a9919828f6b80
RedK012 <casocazzie@outlook.it>
2020-03-21 18:35:12 +0100

    riprova commento boi 4

---
+++ src/progettoscuola/Pc.java
@@ -0,0 +1,41 @@
+package progettoscuola;
+/**
+ *
+ * @author Emanuel
+ */
+public class Pc {
+    private String prezzo;
+    private String Titolo;
+    private String piattaforma;
+
+    public Pc(String prezzo, String Titolo, String piattaforma) {
+        this.prezzo = prezzo;
+        this.Titolo = Titolo;
+        this.piattaforma = piattaforma;
+    }
+
+    public String getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(String prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public String getTitolo() {
+        return Titolo;
+    }
+
+    public void setTitolo(String Titolo) {
+        this.Titolo = Titolo;
+    }
+
+    public String getPiattaforma() {
+        return piattaforma;
+    }
+
+    public void setPiattaforma(String piattaforma) {
+        this.piattaforma = piattaforma;
+    }
+    
+}
