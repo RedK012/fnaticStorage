058a8857cfe8b2470f94378ceaeb73f4a5dc6c42
RedK012 <casocazzie@outlook.it>
2020-03-21 18:35:02 +0100

    riprova commento boi 4

---
+++ src/progettoscuola/Xbox.java
@@ -0,0 +1,58 @@
+
+package progettoscuola;
+
+public class Xbox {
+    private String prezzo;
+    private String Titolo;
+    private String piattaforma;
+
+    public Xbox(String prezzo, String Titolo, String piattaforma) {
+        this.prezzo = prezzo;
+        this.Titolo = Titolo;
+        this.piattaforma = piattaforma;
+    }
+    
+    
+
+    /**
+     * @return the prezzo
+     */
+    public String getPrezzo() {
+        return prezzo;
+    }
+
+    /**
+     * @param prezzo the prezzo to set
+     */
+    public void setPrezzo(String prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    /**
+     * @return the Titolo
+     */
+    public String getTitolo() {
+        return Titolo;
+    }
+
+    /**
+     * @param Titolo the Titolo to set
+     */
+    public void setTitolo(String Titolo) {
+        this.Titolo = Titolo;
+    }
+
+    /**
+     * @return the piattaforma
+     */
+    public String getPiattaforma() {
+        return piattaforma;
+    }
+
+    /**
+     * @param piattaforma the piattaforma to set
+     */
+    public void setPiattaforma(String piattaforma) {
+        this.piattaforma = piattaforma;
+    }
+}
